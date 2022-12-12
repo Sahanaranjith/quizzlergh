5bb0bea6b43f61643780bd3d157e032beaf3d8fc
sahana170498 <sahanammr4@gmail.com>
2022-12-12 13:46:29 +0530

    list related commited

--- lib/main.dart
+++ lib/main.dart
@@ -26,28 +26,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),          
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+       'You can lead a cow down stairs but not up stairs.', 
+       'Approximately one quarter of human bones are in the feet.', 
+       'A slug\'s blood is green.',      
   ];
+  List<bool> answers =[
+    false,
+    true,
+    true
+  ];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -60,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -85,11 +75,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+
+              bool correctAnswer =answers[questionNumber];
+                if(correctAnswer == true){
+                  print('User got correct!');}
+                  else{
+                    print('User is wrong');
+                }
+
                 setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.check,color: Colors.green,),
-                    );
-                });
+                    questionNumber++;
+                });              
               },
             ),
           ),
@@ -109,11 +105,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+
+                 bool correctAnswer =answers[questionNumber];
+                if(correctAnswer == false){
+                  print('User got correct!');}
+                  else{
+                    print('User is wrong');
+                }
+
                 setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.close,color: Colors.red,),
-                    );
-                });
+                    questionNumber++;
+                }); 
               },
             ),
           ),
@@ -125,7 +127,7 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
+// hello
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
